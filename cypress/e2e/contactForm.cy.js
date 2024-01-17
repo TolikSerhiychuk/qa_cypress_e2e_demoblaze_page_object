9e7988858c1cc7dbca77a1f4d2b29c2265085b46
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2024-01-17 17:17:56 +0200

    fifth commit

--- cypress/e2e/contactForm.cy.js
+++ cypress/e2e/contactForm.cy.js
@@ -11,7 +11,7 @@ const testData = {
   email: faker.internet.email(),
   name: faker.name.firstName(),
   message: faker.random.words(),
-  successMessage: 'Thanks for the messages!'
+  successMessage: 'Thanks you!'
 };
 
 describe('Contact', () => {
